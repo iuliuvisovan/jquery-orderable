360e45aa2ef977bfa0d1b72a050e07ed8b1b7960
Iulii <iuliuvisovan@gmail.com>
2016-11-01 20:23:03 +0200

    Fixed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@
 	sourcemaps = require("gulp-sourcemaps");
 
 	gulp.task("minify-css", function() {
-		return gulp.src(['/css/*.css', '!/css/*.min.css'])
+		return gulp.src(['./css/*.css', '!./css/*.min.css'])
 		.pipe(sourcemaps.init())
 		.pipe(cleanCSS())
 		.pipe(rename(function(path) {
@@ -18,7 +18,7 @@
 
 	gulp.task('compress', function (cb) {
 		pump([
-			gulp.src(['/js/*.js', '!/js/*.min.js']),
+			gulp.src(['./js/*.js', '!./js/*.min.js']),
 			uglify(),
 			rename(function(path) {
 				path.basename += ".min";
